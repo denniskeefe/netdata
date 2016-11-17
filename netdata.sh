8ec0d01a183338b164549bef89f2465fe1c5bbb4
dennis <dennis@thecommongeek.com>
2016-11-17 21:30:15 +0000

    update

--- netdata.sh
+++ netdata.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #Simple script to install Netdata on Debian/Ubuntu systems
-
-sudo apt install gcc make autoconf autogen automake pkg-config zlib1g-dev uuid-dev libmnl-dev git &&
+# Access http://your-server-ip:19999
+sudo apt install gcc make autoconf autogen automake pkg-config zlib1g-dev uuid-$
 
 sudo apt install curl jq nodejs &&
 
-git clone https://github.com/firehol/netdata.git && 
+git clone https://github.com/firehol/netdata.git &&
 
 cd netdata
 
 sudo ./netdata-installer.sh
+
 
